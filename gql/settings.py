51ea8aea039ddd8dc82f2a441927948d2f9e6d9c
cloudguruab <ap.brown011@gmail.com>
2022-01-24 21:52:44 -0500

    tutorial update

--- gql/settings.py
+++ gql/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$t^0ro*mu1gg_@k#edv(uuru%qcpg(%rne&eb9#o@2b$$b8#f'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+
+#graphql config 
+GRAPHENE = {
+    "SCHEMA": "gql.schema.schema"
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'gql.api',
 ]
 
 MIDDLEWARE = [
